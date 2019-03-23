fa57f0caaea3ba0c4491d471ca52a3e8dae899cc
profesorjacinto15 <profesor.jacinto15@gmail.com>
2019-03-23 19:18:47 +0000

    Commit Inicial

---
+++ src/factorial.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class factorial {
+    public static int calculo (int n) {
+        if (n < 0) {
+            System.out.println("No puedo Calcular el factorial de un número negativo.");
+        }
+        int fac = 1;
+        for (int i = 2; i <= n; i++) {
+            fac *= i;
+        }
+        return fac;
+    }
+
+    public static void main (String [] args) {
+        Scanner tec = new Scanner(System.in);
+        int numero, facto;
+        System.out.println("Introduce el número al que calcular el factorial: ");
+        numero = tec.nextInt();
+        facto = factorial.calculo(numero);
+        System.out.println("El factorial de " + numero + " es : " + facto);
+    }
+}
