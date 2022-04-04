189c030cf81e05ee148ea5983264ed07c8881a16
profesorjacinto15 <profesor.jacinto15@gmail.com>
2022-04-04 22:35:46 +0100

    Commit inicial

---
+++ src/Factorial.java
@@ -0,0 +1,65 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+
+/**
+ * @author jacinto
+ *
+ */
+
+public class Factorial {
+
+/***
+ * 	
+ * @param valor
+ */
+
+    public static void imprime (Object valor) {
+        System.out.println(valor);
+    }
+    
+    /***
+     *  
+     * @return numero 
+     */
+    
+    public static int leerNumero() {
+        Scanner teclado = new Scanner(System.in);
+        int numero;
+        do {
+            imprime("Indique un número entero positivo: ");
+            numero = teclado.nextInt();
+        } while (numero < 0);
+        return numero;
+    }
+/***
+ * 
+ * @param numero
+ * @return
+ */
+
+    public static int factorial(int numero) {
+        if (numero == 0) {
+            return 1;
+        } else {
+            int resultado = numero;
+            for (int valor = numero-1; valor >= 1; valor--) {
+                resultado *= valor;
+            }        
+            return resultado;
+        }
+    }
+  
+    /***
+     * 
+     * @param args
+     * @throws IOException
+     */
+    
+    public static void main(String[] args) throws IOException {
+        imprime("CÁLCULO DEL FACTORIAL DE UN NÚMERO");
+        int numero = leerNumero();        
+        int resultado = factorial(numero);
+        imprime("El factorial de " + numero + " es " + resultado);
+    }    
+}
